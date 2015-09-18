e1031f15adc40fa0553b42c7e5a5660f3904eaf9
masterUNG <phrombutr@gmail.com>
2015-09-18 13:44:28 +0700

    Update Value to MySQL Finish

--- app/src/main/java/kmutnb/yuwadee/srisawat/carpool/ShowLatLngActivity.java
+++ app/src/main/java/kmutnb/yuwadee/srisawat/carpool/ShowLatLngActivity.java
@@ -7,11 +7,24 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class ShowLatLngActivity extends AppCompatActivity {
 
@@ -21,6 +34,7 @@ public class ShowLatLngActivity extends AppCompatActivity {
     private Criteria objCriteria;
     private double latADouble, lngADouble;
     private boolean GPSABoolean, NetworkABoolean;
+    private String strName, strSurname, strTable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -144,12 +158,12 @@ public class ShowLatLngActivity extends AppCompatActivity {
     private void showView() {
 
         //Show Welcome
-        String strTable = getIntent().getStringExtra("Table");
+        strTable = getIntent().getStringExtra("Table");
         tableTextView.setText("Welcome " + strTable);
 
         //Show User
-        String strName = getIntent().getStringExtra("Name");
-        String strSurname = getIntent().getStringExtra("Surname");
+        strName = getIntent().getStringExtra("Name");
+        strSurname = getIntent().getStringExtra("Surname");
         userTextView.setText(strName + " " + strSurname);
 
     }
@@ -162,6 +176,9 @@ public class ShowLatLngActivity extends AppCompatActivity {
     }
 
     public void clickGoToMap(View view) {
+
+        //Check Value
+        checkValue();
 
         //Get Lat,Lng
         Double latDouble = Double.parseDouble(latTextView.getText().toString());
@@ -178,6 +195,55 @@ public class ShowLatLngActivity extends AppCompatActivity {
 
 
     }   // clickGoToMap
+
+    private void checkValue() {
+
+        //Get Lat,Lng
+        String strLat = latTextView.getText().toString();
+        String strLng = lngTextView.getText().toString();
+
+        //Get Time
+        DateFormat myDateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date currentDate = new Date();
+        String strDate = myDateFormat.format(currentDate);
+
+
+        Log.d("Map", "Name = " + strName);
+        Log.d("Map", "Surname = " + strSurname);
+        Log.d("Map", "Icon = " + strTable);
+        Log.d("Map", "Lat = " + strLat);
+        Log.d("Map", "Lng = " + strLng);
+        Log.d("Map", "Date = " + strDate);
+
+        //Update Value to mySQL
+
+        StrictMode.ThreadPolicy myPolicy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(myPolicy);
+
+        try {
+
+            ArrayList<NameValuePair> objNameValuePairs = new ArrayList<NameValuePair>();
+            objNameValuePairs.add(new BasicNameValuePair("isAdd", "true"));
+            objNameValuePairs.add(new BasicNameValuePair("Name", strName));
+            objNameValuePairs.add(new BasicNameValuePair("Surname", strSurname));
+            objNameValuePairs.add(new BasicNameValuePair("Icon", strTable));
+            objNameValuePairs.add(new BasicNameValuePair("Lat", strLat));
+            objNameValuePairs.add(new BasicNameValuePair("Lng", strLng));
+            objNameValuePairs.add(new BasicNameValuePair("Date", strDate));
+
+            HttpClient objHttpClient = new DefaultHttpClient();
+            HttpPost objHttpPost = new HttpPost("http://swiftcodingthai.com/joy/php_add_data_maps.php");
+            objHttpPost.setEntity(new UrlEncodedFormEntity(objNameValuePairs, "UTF-8"));
+            objHttpClient.execute(objHttpPost);
+
+            Toast.makeText(ShowLatLngActivity.this, "Update mySQL Success", Toast.LENGTH_SHORT).show();
+
+        } catch (Exception e) {
+            Toast.makeText(ShowLatLngActivity.this, "Cannot Update mySQL ?", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }   // checkValue
 
 
 }   // Main Class
